60c127dad8d99d34ca324229bccda523b45837ef
Kevin <KevinKYuen@gmail.com>
2014-10-10 17:52:44 -0400

    added checker to see venv is installed yet

--- provision.sh
+++ provision.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
+if [ -d "/vagrant/contrajd/venv" ]
+then
+   cd /vagrant/contrajd
+   . venv/bin/activate
 
-cd /vagrant/
-sudo apt-get -y install python python-pip git 
-sudo pip install virtualenv
-cd contrajd
-virtualenv venv
+   python app &
+   
+else
+	cd /vagrant/
+	sudo apt-get -y install python python-pip git 
+	sudo pip install virtualenv
+	cd contrajd
+	virtualenv venv
 
-. venv/bin/activate
+	. venv/bin/activate
 
-pip install Flask
+	pip install Flask
 
-python app.py &
+	python app.py &
+fi
+
+
+
 
