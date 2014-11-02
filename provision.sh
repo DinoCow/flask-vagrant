d875e45329543cc9e097e8ea3635b231776cb67c
Kevin <KevinKYuen@gmail.com>
2014-11-02 14:03:14 -0500

    added new provisioning with modules to support pdf formfilling

--- provision.sh
+++ provision.sh
@@ -7,17 +7,23 @@ then
    
 else
 	cd /vagrant/
-	sudo apt-get -y install python python-pip 
+	sudo apt-get -y install python python-pip pdftk
 	cd contrajd
 
 	sudo pip install Flask 
 	sudo pip install requests
 	sudo pip install -U flask-cors
-
+	sudo pip install boto
+	sudo pip install fdfgen
 
 	sudo pip install requests --upgrade
 	
 	sudo mkdir "/installed" 
+	mkdir "/tmp/form"
+	mkdir "/tmp/pdf"
+	mkdir "/tmp/fdf"
 
+	sudo chmod 777 /tmp/form /tmp/pdf tmp/fdf
+	cp /vagrant/*.pdf /tmp/form
 	python app.py &
 fi
